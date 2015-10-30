22ff478a8c4d2215d70746ab7a72d053cab43aca
Jeseekia Vaughn <jeseekia@gmail.com>
2015-10-30 05:45:47 -0400

    Just adding a few comments to the code.

--- video-game/frogs-n-logs/Assets/Scripts/PlayerMovement.cs
+++ video-game/frogs-n-logs/Assets/Scripts/PlayerMovement.cs
@@ -10,22 +10,31 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Gather the Animator component from the player game object
 		playerAnimator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Gather input from the keyboard
 		moveHorizontal = Input.GetAxisRaw("Horizontal");
 		moveVertical = Input.GetAxisRaw ("Vertical");
 
 		movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
 	}
 	// Runs just after Update method but not as often as update
 	void FixedUpdate () {
+
+		// If the player's movement vector doesn't equal 0
 		if (movement != Vector3.zero) {
+			//...then play the jump animation
 			playerAnimator.SetFloat ("Speed", 3f);
 		} else {
+			//...otherwise don't play the jump animation
 			playerAnimator.SetFloat ("Speed", 0f);
 		}
+
 	}
 }
